5d563948f7c42af427d8427ba588a5576df5fe7c
yoo chang heon <dbckdgjs1130@naver.com>
2024-10-06 16:56:45 +0900

    design: 버튼 hover 효과 추가

--- src/lib/components/Button/styled.tsx
+++ src/lib/components/Button/styled.tsx
@@ -1,10 +1,12 @@
 import styled from "@emotion/styled";
 
 export const StyledButton = styled.button`
-  background-color: transparent;
   border: 1px solid;
   width: 100px;
   height: 42px;
   padding: 8px;
   border-radius: 4px;
+  &:hover {
+    filter: brightness(0.8);
+  }
 `;
